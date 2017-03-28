f95b6ca0c9b3566ae1ac2308d8eb81dc214c38d5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-03-28 23:22:30 +0200

    Update analytics.pp

--- manifests/analytics.pp
+++ manifests/analytics.pp
@@ -2,6 +2,11 @@
 #
 class role_postgresql::analytics {
 
+  # Contrib package for pg_stat_statements, pg_buffercache
+  class { 'postgresql::server::contrib':
+    package_name => 'postgresql-contrib'
+  }
+
   # Postgres analytics scripts
   file { '/opt/postgresql':
     source  => 'puppet:///modules/role_postgresql',
