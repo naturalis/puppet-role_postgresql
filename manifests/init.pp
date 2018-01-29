489d2ea0617a8017608b15c1c1fd3bcc7842252a
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-01-29 22:44:50 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -90,16 +90,16 @@ class role_postgresql (
   }
 
   # Remote connections
-  $pg_hba_rule_hash.each |$name, $pg_hba_rule| {
-    postgresql::server::pg_hba_rule { $name:
-      description  => $pg_hba_rule["description"],
-      type         => $pg_hba_rule["type"],
-      database     => $pg_hba_rule["database"],
-      user         => $pg_hba_rule["user"],
-      address      => $pg_hba_rule["address"],
-      auth_method  => $pg_hba_rule["auth_method"],
-    }
-  }
+  #$pg_hba_rule_hash.each |$name, $pg_hba_rule| {
+  #  postgresql::server::pg_hba_rule { $name:
+  #    description  => $pg_hba_rule["description"],
+  #    type         => $pg_hba_rule["type"],
+  #    database     => $pg_hba_rule["database"],
+  #    user         => $pg_hba_rule["user"],
+  #    address      => $pg_hba_rule["address"],
+  #    auth_method  => $pg_hba_rule["auth_method"],
+  #  }
+  #}
 
   # Configure options in postgresql.conf
   $config_values.each |$key, $value| {
