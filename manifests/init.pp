715de73354d2e156982c81b5a47293f73a6d0565
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-01-29 11:40:13 +0100

    dynamic effective_cache_size

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,7 +44,9 @@ class role_postgresql (
   $analytics             = true,
   $cron_job_hash         = undef,
   $config_values_static  = undef,
-  $config_values_dynamic = { 'shared_buffers' => $facts['memory']['system']['total_bytes'] * 3/4 },
+  $config_values_dynamic = { 'shared_buffers'       => $facts['memory']['system']['total_bytes'] * 3/4,
+                             'effective_cache_size' => $facts['memory']['system']['total_bytes'] * 3/4
+                           },
   $config_values         = deep_merge($config_values_static, $config_values_dynamic)
   ) {
 
